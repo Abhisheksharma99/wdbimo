3bb31773dd558ca0d84b9c87944697445100393a
Abhishek <abhisheksharma999r@gmail.com>
2024-10-17 13:11:25 +0530

    cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,16 @@ import Services from './components/Services'
 import Clients from './components/Clients'
 import Contact from './components/Contacts'
 import Footer from './components/Footer'
+import AboutUs from './components/AboutUs'
+import OurBusinesses from './components/OurBusiness'
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100">
       <Header />
       <Hero />
+      <AboutUs />
+      <OurBusinesses />
       <Services />
       <Clients />
       <Contact />
